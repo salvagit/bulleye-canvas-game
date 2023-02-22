8c9f3f5227693e3b91397776ac327ef125b3aa0f
salva <s@salva.io>
2023-02-21 22:27:14 -0300

    chore: move size as canvas attr.

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,9 @@
 import { Game } from "./classes/Game.js";
-import { Player } from "./classes/Player.js";
 
 window.addEventListener("load", () => {
   const canvas = document.querySelector(".canvas");
 
   const ctx = canvas.getContext("2d");
-
-  canvas.width = 1280;
-  canvas.height = 720;
 
   ctx.fillStyle = "white";
   ctx.lineWidth = 3;
