e7a1ff41c3446f177ac24592a392c681cf1d0eec
salva <s@salva.io>
2023-02-25 20:11:39 -0300

    feat: add player sprite.

--- scripts/classes/Player.js
+++ scripts/classes/Player.js
@@ -1,19 +1,50 @@
 class Player {
   constructor(game) {
     this.game = game;
-    this.collisionX = this.game.width * .5;
-    this.collisionY = this.game.height * .5;
+    this.collisionX = this.game.width * 0.5;
+    this.collisionY = this.game.height * 0.5;
     this.collisionRadius = 30;
     this.speedX = 0;
     this.speedY = 0;
     this.dx = 0;
     this.dy = 0;
-    this.speedModifier = 5;
+    this.speedModifier = 3;
+
+    this.spriteWidth = 255;
+    this.spriteHeight = 255;
+
+    this.spriteX;
+    this.spriteY;
+
+    this.frameX = 0;
+    this.frameY = 0;
+
+    this.width = 255;
+    this.height = 255;
+
+    this.image = document.querySelector(".bull");
   }
 
   draw(context) {
+    context.drawImage(
+      this.image,
+      this.frameX * this.spriteWidth,
+      this.frameY * this.spriteHeight,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.spriteX,
+      this.spriteY,
+      this.width,
+      this.height
+    );
     context.beginPath();
-    context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI *  2);
+    context.arc(
+      this.collisionX,
+      this.collisionY,
+      this.collisionRadius,
+      0,
+      Math.PI * 2
+    );
 
     context.save();
     context.globalAlpha = 0.5;
@@ -21,7 +52,7 @@ class Player {
     context.restore();
 
     context.stroke();
-    
+
     context.beginPath();
     context.moveTo(this.collisionX, this.collisionY);
     context.lineTo(this.game.mouse.x, this.game.mouse.y);
@@ -32,6 +63,18 @@ class Player {
     this.dx = this.game.mouse.x - this.collisionX;
     this.dy = this.game.mouse.y - this.collisionY;
 
+    // sprite animation.
+    const angle = Math.atan2(this.dy, this.dx);
+
+    if (angle < -2.74 || angle > 2.74) this.frameY = 6;
+    else if (angle < -1.96) this.frameY = 7;
+    else if (angle < -1.17) this.frameY = 0;
+    else if (angle < -0.39) this.frameY = 1;
+    else if (angle < 0.39) this.frameY = 2;
+    else if (angle < 1.17) this.frameY = 3;
+    else if (angle < 1.96) this.frameY = 4;
+    else if (angle < 2.74) this.frameY = 5;
+    
     const distance = Math.hypot(this.dy, this.dx);
 
     if (distance > this.speedModifier) {
@@ -45,17 +88,20 @@ class Player {
     this.collisionX += this.speedX * this.speedModifier;
     this.collisionY += this.speedY * this.speedModifier;
 
+    this.spriteX = this.collisionX - this.width * 0.5;
+    this.spriteY = this.collisionY - this.height * 0.5;
+
     // collisions with obstacles.
-    this.game.obstacles.forEach(obstacle => {
-      const [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, obstacle)
+    this.game.obstacles.forEach((obstacle) => {
+      const [collision, distance, sumOfRadii, dx, dy] =
+        this.game.checkCollision(this, obstacle);
+
       if (collision) {
         const unit_x = dx / distance;
         const unit_y = dy / distance;
 
         this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x;
         this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
-
-        console.log('Colichioni');
       }
     });
   }
